a05c2e967ec3a5f8a0bb87ea424436e97c4458a4
lshdainty <lshdainty@gmail.com>
2025-10-10 16:38:05 +0900

    input autofill시 background color가 이상해지는 현상 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
