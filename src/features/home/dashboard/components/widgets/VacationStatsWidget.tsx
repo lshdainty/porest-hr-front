8b991a6f8a020f0d0f26e157d0ab3eb3e1c1c539
lshdainty <lshdainty@gmail.com>
2025-11-29 13:04:35 +0900

    모바일 화면 대응 수정

--- src/features/home/dashboard/components/widgets/VacationStatsWidget.tsx
+++ src/features/home/dashboard/components/widgets/VacationStatsWidget.tsx
@@ -14,9 +14,9 @@ const VacationStatsWidget = ({ vacationStats }: VacationStatsWidgetProps) => {
   const statsConfig = getVacationStatsConfig(vacationStats);
 
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-3 gap-px bg-border h-full'>
+    <div className='grid grid-cols-1 lg:grid-cols-3 divide-y lg:divide-y-0 lg:divide-x divide-border h-full bg-card'>
       {statsConfig.map((item) => (
-        <div key={item.id} className='p-6 bg-card h-full'>
+        <div key={item.id} className='p-4 h-full'>
           <VacationStatsItem {...item} />
         </div>
       ))}
